7a9fedb601b85439c2e27ab61f4cde3ac2799661
Raivo Laanemets <raivo@infdot.com>
2013-11-28 06:48:23 +0200

    Prevent daemon initialization multiple times, closes #23

--- prolog/bc/bc_daemon.pl
+++ prolog/bc/bc_daemon.pl
@@ -3,11 +3,23 @@
 ]).
 
 :- use_module(library(http/http_unix_daemon)).
-
 :- use_module(bc_init).
 
+% Flag to prevent initialization
+% multiple times.
+
+:- dynamic(initialized).
+
+%% bc_init_daemon is det.
+%
+% Daemonizes the currently running
+% server by calling http_daemon/1.
+
 bc_init_daemon:-
-    http_daemon.
+    (   initialized
+    ->  true
+    ;   http_daemon,
+        assertz(initialized)).
 
 % Sets up the hook that calls bc_setup/1.
 
