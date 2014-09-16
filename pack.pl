ce645d4c36c55354d0e53b50fef3621d88ec2b07
Raivo Laanemets <raivo@infdot.com>
2014-09-16 19:24:51 +0300

    The pack.pl file and dependencies, closes #65

---
+++ pack.pl
@@ -0,0 +1,11 @@
+name('blog_core').
+version('0.0.1').
+title('Blog/CMS framework').
+author('Raivo Laanemets', 'http://rlaanemets.com/').
+home('https://github.com/rla/blog-core').
+requires('docstore').
+requires('arouter').
+requires('simple_template').
+requires('sort_dict').
+requires('dict_schema').
+requires('markdown').
