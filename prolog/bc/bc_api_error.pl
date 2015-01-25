429aebe58dc7215eab0a1f7239cb883929490256
Raivo Laanemets <raivo@infdot.com>
2015-01-25 04:48:44 +0200

    Clean up bc_api_error from unused errors, closes #102

--- prolog/bc/bc_api_error.pl
+++ prolog/bc/bc_api_error.pl
@@ -22,8 +22,6 @@ bc_call_handle_error(Goal):-
 % Handles known errors and produces
 % approriate API response.
 
-% FIXME move into API modules?
-
 bc_handle_error(error(invalid_api_key)):- !,
     bc_reply_error('Invalid or missing API key.').
 
@@ -31,14 +29,15 @@ bc_handle_error(error(invalid_input(Errors))):- !,
     format(atom(Message), 'Invalid input: ~w.', [Errors]),
     bc_reply_error(Message).
 
+bc_handle_error(error(no_config(Name))):- !,
+    format(atom(Message), 'Invalid config key: ~w.', [Name]),
+    bc_reply_error(Message).
+
 bc_handle_error(error(invalid_credentials)):- !,
     bc_reply_error('Invalid auth credentials.').
 
 bc_handle_error(error(no_login_access)):- !,
     bc_reply_error('Assigned role does not permit login.').
-
-bc_handle_error(error(user_password_is_not_set)):- !,
-    bc_reply_error('The user password is not set.').
 
 bc_handle_error(error(invalid_username)):- !,
     bc_reply_error('The username is invalid.').
@@ -48,9 +47,6 @@ bc_handle_error(error(existing_username)):- !,
 
 bc_handle_error(error(invalid_role)):- !,
     bc_reply_error('The user role is not valid.').
-
-bc_handle_error(error(user_current_is_not_admin)):- !,
-    bc_reply_error('The operation requires admin privileges.').
 
 bc_handle_error(error(user_not_exists)):- !,
     bc_reply_error('The user does not exist.').
@@ -67,12 +63,6 @@ bc_handle_error(error(existing_slug)):- !,
 bc_handle_error(error(entry_not_exists)):- !,
     bc_reply_error('The entry does not exist.').
 
-bc_handle_error(error(no_ownership)):- !,
-    bc_reply_error('The operation requires ownership privileges.').
-
-bc_handle_error(error(no_type_access)):- !,
-    bc_reply_error('The operation requires entry type access.').
-
 bc_handle_error(error(no_access)):- !,
     bc_reply_error('The operation requires access privileges.').
 
@@ -85,23 +75,14 @@ bc_handle_error(error(directory_exists)):- !,
 bc_handle_error(error(file_exists)):- !,
     bc_reply_error('The file exists.').
 
-bc_handle_error(error(no_files_access)):- !,
-    bc_reply_error('No permission to manage files.').
-
 bc_handle_error(error(incorrect_answer)):- !,
     bc_reply_error('The human question answer is wrong.').
 
 bc_handle_error(error(no_comments_allowed)):- !,
     bc_reply_error('No comments are allowed on the entry.').
 
-bc_handle_error(error(comment_reply_wrong_post)):- !,
-    bc_reply_error('The replied-to comment and the entry do not match.').
-
 bc_handle_error(error(comment_not_exists)):- !,
     bc_reply_error('The comment replied to does not exist.').
-
-bc_handle_error(error(reply_post_id_mismatch(_))):- !,
-    bc_reply_error('The comment replied to does not match the post.').
 
 bc_handle_error(error(invalid_slug)):- !,
     bc_reply_error('The slug is invalid.').
